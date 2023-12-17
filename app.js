cf7f1b54c03f253e9218bff54f9db8926572c821
isPoto <plo@vivaldi.net>
2023-12-17 12:20:48 +0800

    1

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function download() {
     // window.location.href = 'https://github.com/isPoto/SingbitP/releases/download/0/SingbitP.exe'
     // window.location.href = 'https://mirror.ghproxy.com/https://github.com/isPoto/SingbitP/releases/download/0/SingbitP.exe'
-    window.location.href = 'https://github.abskoop.workers.dev/https://github.com/isPoto/SingbitP/releases/download/0/SingbitP.exe'
+    window.location.href = 'https://github.com/isPoto/SingbitP/releases/download/0/SingbitP.exe'
 
 }
